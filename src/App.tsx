5d3f7737c3a3107e572c69b804041a3708a655c7
jiwoon-ko <jiwoonko214@gmail.com>
2026-07-17 01:56:43 +0900

    Revert "Update App.tsx"
    This reverts commit 16cc69647e66f56c8678fe842c19615e33d2c69b.

--- src/App.tsx
+++ src/App.tsx
@@ -148,7 +148,7 @@ function App() {
             <span />
           </span>
           <span>
-            GRAY<em>HACKS</em>
+            SENTINEL<em>ARENA</em>
           </span>
         </button>
         <div className="nav-label">WORKSPACE</div>
@@ -660,7 +660,7 @@ function Arena({
                 <Icon name="spark" size={16} />
               </span>
               <span>
-                <b>Gray Coach</b>
+                <b>Sentinel Coach</b>
                 <small>힌트 기반 안내</small>
               </span>
               <span>{coachOpen ? "−" : "+"}</span>
